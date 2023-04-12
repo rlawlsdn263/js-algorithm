458246792dd31577f36d51c89c8163b47f9ebda4
rlawlsdn263 <rlawlsdn263@naver.com>
2023-04-12 09:00:51 +0900

    [💯해결] lv0 두 수의 차
    - - 연산자를 사용하면 뺀 값을 구할 수 있음

---
+++ programmers/level0/03.두 수의 차/README.md
@@ -0,0 +1,3 @@
+# 두 수의 차
+
+출처: https://school.programmers.co.kr/learn/courses/30/lessons/120803
---
+++ programmers/level0/03.두 수의 차/해설.js
@@ -0,0 +1,4 @@
+function solution(num1, num2) {
+  // - 기호를 사용하면 두 수의 차를 구할 수 있음
+  return num1 - num2;
+}
