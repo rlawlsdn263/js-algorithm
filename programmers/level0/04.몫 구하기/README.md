feea9af03ea32f8ce10bd8e90fa5ea4cf877b070
rlawlsdn263 <rlawlsdn263@naver.com>
2023-04-12 09:32:26 +0900

    [💯해결] lv0 몫 구하기
    - Math.floor()와 parseInt()로 소수점을 제거한 값을 구할 수 있음

---
+++ programmers/level0/04.몫 구하기/README.md
@@ -0,0 +1,3 @@
+# 몫 구하기
+
+출처: https://school.programmers.co.kr/learn/courses/30/lessons/120805
---
+++ programmers/level0/04.몫 구하기/풀이.js
@@ -0,0 +1,7 @@
+function solution(num1, num2) {
+  //Math.floor()를 사용해 소수점을 제거함
+  return Math.floor(num1 / num2);
+}
+
+console.log(solution(10, 5));
+console.log(solution(7, 2));
