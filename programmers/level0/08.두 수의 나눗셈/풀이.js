f311167a6315bdb4cae0ab378b888aeba6fa6232
rlawlsdn263 <rlawlsdn263@naver.com>
2023-04-13 10:27:49 +0900

    [💯해결] lv0 두 수의 나눗셈
    - Math.trunc()는 주어진 값이 양수이건 음수이건 상관없이 소수점 이하 우측부분을 제거해줌

---
+++ programmers/level0/08.두 수의 나눗셈/풀이.js
@@ -0,0 +1,8 @@
+const num1 = 12;
+const num2 = 1;
+
+function solution(num1, num2) {
+  return Math.trunc((num1 / num2) * 1000);
+}
+
+console.log(solution(num1, num2));
---
+++ programmers/level0/08.두 수의 나눗셈/풀이2.js
@@ -0,0 +1,8 @@
+const num1 = 3;
+const num2 = 2;
+
+function solution(num1, num2) {
+  return Math.floor((num1 / num2) * 1000);
+}
+
+console.log(solution(num1, num2));
