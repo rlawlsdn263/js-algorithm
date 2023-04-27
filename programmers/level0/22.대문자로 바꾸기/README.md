99e63da3d35f2db6d2aa5c76ab210912b2f5ee14
rlawlsdn263 <rlawlsdn263@naver.com>
2023-04-27 10:03:15 +0900

    [💯풀이] lv0 대문자로 바꾸기
    - toUpperCase()를 사용해 소문자를 대문자로 변환함

---
+++ programmers/level0/22.대문자로 바꾸기/README.md
@@ -0,0 +1,3 @@
+# 대문자로 바꾸기
+
+출처: https://school.programmers.co.kr/learn/courses/30/lessons/181877
---
+++ programmers/level0/22.대문자로 바꾸기/풀이.js
@@ -0,0 +1,9 @@
+let myString = "aBcDeFg";
+let myString2 = "AAA";
+
+function solution(myString) {
+  return myString.toUpperCase();
+}
+
+console.log(solution(myString));
+console.log(solution(myString2));
