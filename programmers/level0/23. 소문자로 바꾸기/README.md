1ec3b26ff5783444a0791d42c148885e47cd7674
rlawlsdn263 <rlawlsdn263@naver.com>
2023-04-27 10:06:56 +0900

    [💯풀이] lv0 소문자로 바꾸기
    - toLowerCase()를 사용해 대문자를 소문자로 변환함

---
+++ programmers/level0/23. 소문자로 바꾸기/README.md
@@ -0,0 +1,3 @@
+# 소문자로 바꾸기
+
+출처: https://school.programmers.co.kr/learn/courses/30/lessons/181876
---
+++ programmers/level0/23. 소문자로 바꾸기/풀이.js
@@ -0,0 +1,9 @@
+let myString = "aBcDeFg";
+let myString2 = "aaa";
+
+function solution(myString) {
+  return myString.toLowerCase();
+}
+
+console.log(solution(myString));
+console.log(solution(myString2));
