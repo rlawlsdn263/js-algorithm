02e149165f2c8a6eba7ab670396b00f3cfcf6e09
rlawlsdn263 <rlawlsdn263@naver.com>
2023-04-27 10:52:02 +0900

    [💯풀이] lv0 문자열을 정수로 변환하기
    - Number()를 사용해서 문자열을 숫자형으로 변환했음
    - 정수를 변환하는 용도로는 parseInt()가 더 적합할 수 있음

---
+++ programmers/level0/24.문자열을 정수로 변환하기/README.md
@@ -0,0 +1,3 @@
+# 문자열을 정수로 변환하기
+
+출처: https://school.programmers.co.kr/learn/courses/30/lessons/181848
---
+++ programmers/level0/24.문자열을 정수로 변환하기/풀이.js
@@ -0,0 +1,9 @@
+let n_str = "10";
+let n_str2 = "8542";
+
+function solution(n_str) {
+  return Number(n_str);
+}
+
+console.log(solution(n_str));
+console.log(solution(n_str2));
