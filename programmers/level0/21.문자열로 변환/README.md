c7a5e73027025660bd66d014fb1175efce04f06f
rlawlsdn263 <rlawlsdn263@naver.com>
2023-04-27 09:59:45 +0900

    [💯풀이] lv0 문자열로 변환
    - String()을 사용해 숫자형을 문자형으로 변환함
    - String()은 toString()과 다르게 null과 undefined가 나올 경우에도 에러가 발생하지 않음

---
+++ programmers/level0/21.문자열로 변환/README.md
@@ -0,0 +1,3 @@
+# 문자열로 변환
+
+출처: https://school.programmers.co.kr/learn/courses/30/lessons/181845
---
+++ programmers/level0/21.문자열로 변환/풀이.js
@@ -0,0 +1,8 @@
+let n = 123;
+let n2 = 2573;
+
+function solution(n) {
+  return String(n);
+}
+
+console.log(solution(n));
