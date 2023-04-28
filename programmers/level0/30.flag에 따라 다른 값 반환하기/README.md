b780bfcc855bf3c2a00294c0015887dd9c67adde
rlawlsdn263 <rlawlsdn263@naver.com>
2023-04-28 09:58:45 +0900

    [💯풀이] lv0 flag에 따라 다른 값 반환하기

---
+++ programmers/level0/30.flag에 따라 다른 값 반환하기/README.md
@@ -0,0 +1,3 @@
+# flag에 따라 다른 값 반환하기
+
+출처: https://school.programmers.co.kr/learn/courses/30/lessons/181933
---
+++ programmers/level0/30.flag에 따라 다른 값 반환하기/풀이.js
@@ -0,0 +1,10 @@
+let a = -4;
+let b = 7;
+let flag = true;
+
+function solution(a, b, flag) {
+  if (flag) return a + b;
+  else return a - b;
+}
+
+console.log(solution(a, b, flag));
