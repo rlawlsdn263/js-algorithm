53e3405af942cfe317e38278eb92ca48a8884282
rlawlsdn263 <rlawlsdn263@naver.com>
2023-05-04 18:53:27 +0900

    [💯풀이] lv0 공백으로 구분짓기1
    - Array.split()의 파라미터로 공백을 주면 문자를 공백에 따라 배열로 변환함

---
+++ programmers/level0/34.공백으로 구분하기1/README.md
@@ -0,0 +1,3 @@
+# 공백으로 구분하기1
+
+출처: https://school.programmers.co.kr/learn/courses/30/lessons/181869
---
+++ programmers/level0/34.공백으로 구분하기1/풀이.js
@@ -0,0 +1,9 @@
+let my_string = "i love you";
+let my_string2 = "programmers";
+
+function solution(my_string) {
+  return my_string.split(" ");
+}
+
+console.log(solution(my_string));
+console.log(solution(my_string2));
