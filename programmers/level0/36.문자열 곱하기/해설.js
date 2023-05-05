6eeed78401f5ab765acd490153f0bd2fec8b6507
rlawlsdn263 <rlawlsdn263@naver.com>
2023-05-05 14:26:37 +0900

    [🥇해설] lv0 문자열 곱하기
    - String.repeat()을 사용하면 원하는 만큼 문자를 이어붙일 수 있음

---
+++ programmers/level0/36.문자열 곱하기/해설.js
@@ -0,0 +1,4 @@
+function solution(my_string, k) {
+  //String.repeat()은 주어진 개수만큼 박복해준다
+  return my_string.repeat(k);
+}
---
+++ 같은 숫자는 싫어/README.md
@@ -0,0 +1,3 @@
+# 같은 숫자는 싫어
+
+출처: https://school.programmers.co.kr/learn/courses/30/lessons/12906
